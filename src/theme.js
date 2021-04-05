83241390bdff2ec311d2cbca38a2004bcf660339
Artem-Rudko <72283896+Artem-Rudko@users.noreply.github.com>
2021-04-06 01:39:24 +0300

    add correction by adding theme-change-fn

--- src/theme.js
+++ src/theme.js
@@ -8,15 +8,23 @@ const Theme = {
 
 savedThemeCheck();
 
+function themeColorChange(remove,add) {
+    bodyEl.classList.remove(remove);
+    bodyEl.classList.add(add);
+    localStorage.setItem('theme',add);
+}
+
 function onThemeToggleChange(event) {
     if (themeToggleEl.checked) {
-        localStorage.setItem('theme',Theme.DARK);
-        bodyEl.classList.add(Theme.DARK);
-        bodyEl.classList.remove(Theme.LIGHT);
+        // localStorage.setItem('theme',Theme.DARK);
+        themeColorChange(Theme.LIGHT, Theme.DARK);
+        // bodyEl.classList.add(Theme.DARK);
+        // bodyEl.classList.remove(Theme.LIGHT);
     } else {
-        localStorage.setItem('theme',Theme.LIGHT);
-        bodyEl.classList.add(Theme.LIGHT);
-        bodyEl.classList.remove(Theme.DARK);
+        // localStorage.setItem('theme',Theme.LIGHT);
+        themeColorChange(Theme.DARK, Theme.LIGHT);
+        // bodyEl.classList.add(Theme.LIGHT);
+        // bodyEl.classList.remove(Theme.DARK);
     }
 };
 
